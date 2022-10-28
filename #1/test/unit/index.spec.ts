eed5e82a622554dc964998506c1e30e3ea7a3f44
djansyle <djansyledjans@gmail.com>
2022-10-28 18:48:59 +0800

    fix: fix typo on expected balance

--- #1/test/unit/index.spec.ts
+++ #1/test/unit/index.spec.ts
@@ -5,7 +5,7 @@ import { getAccountInformation, calculateAccountBalance } from '../../src';
 describe('#calculateAccountBalance', function () {
   it('SHOULD return the correct balance of the account', function () {
     [
-      { account: '60329145-ba86-44fb-8fc8-519e1e427a60', balance: 27 },
+      { account: '60329145-ba86-44fb-8fc8-519e1e427a60', balance: 23 },
       { account: 'd5dedb98-1894-4cf5-9b42-edb755b16f04', balance: 0 }
     ].forEach((data) => {
       expect(calculateAccountBalance(AccountEvents, data.account)).to.eql(data.balance);
