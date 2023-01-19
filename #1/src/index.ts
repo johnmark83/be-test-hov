6e7789033fdd9c49c36c3c38ffc540112006fc24
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-19 13:23:14 +0800

    perf: use switch instead of if

--- #1/src/index.ts
+++ #1/src/index.ts
@@ -1,23 +1,25 @@
 import { AccountEvents } from '../../events';
 
-function _isIdAndTypeEqual(id: string, type: string, itemId: string, itemType: string) {
-  return id === itemId && type === itemType;
-}
-
 export function calculateAccountBalance(events: typeof AccountEvents, accountId: string): number {
-  const totalCredit: number = events.reduce((acc, curr) => {
-    if (_isIdAndTypeEqual(accountId, "BalanceCredited", curr.aggregateId, curr.type)) return acc += curr.body.amount ?? 0;
+
+  const totalBalance: number = events.reduce((acc, curr) => {
+    if (accountId !== curr.aggregateId) return acc;
+
+    switch (curr.type) {
+      case "BalanceCredited":
+        acc += curr.body.amount ?? 0;
+        break;
+      case "BalanceDebited":
+        acc -= curr.body.amount ?? 0;
+        break;
+      default:
+        break;
+    }
 
     return acc;
   }, 0);
 
-  const totalDebit: number = events.reduce((acc, curr) => {
-    if (_isIdAndTypeEqual(accountId, "BalanceDebited", curr.aggregateId, curr.type)) return acc += curr.body.amount ?? 0;
-
-    return acc;
-  }, 0);
-
-  return totalCredit - totalDebit;
+  return totalBalance;
 }
 
 export function getAccountInformation(events: typeof AccountEvents, accountId: string) {
