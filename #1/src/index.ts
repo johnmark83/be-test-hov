4eaee401891495d944692fd73a82e87c053337a9
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-19 19:37:30 +0800

    refactor: use short-hand notation for function

--- #1/src/index.ts
+++ #1/src/index.ts
@@ -27,7 +27,7 @@ export function getAccountInformation(events: typeof AccountEvents, accountId: s
   const accountInformation: AccountInformation | null = events.reduce((acc: AccountInformation | null, curr: Event) => {
 
     let aggregateFunction = {
-      "AccountCreated": function () {
+      "AccountCreated": () => {
         if (curr.aggregateId !== accountId) return;
         acc = (body => (
           {
@@ -37,23 +37,23 @@ export function getAccountInformation(events: typeof AccountEvents, accountId: s
           } as AccountInformation)
         )(curr.body);
       },
-      "AccountUpdated": function () {
+      "AccountUpdated": () => {
         if (acc === null || curr.aggregateId !== accountId) return;
         Object.keys(curr.body).forEach(key => {
           acc![key] = curr.body[key];
         });
       },
-      "WithdrawalCreated": function () {
+      "WithdrawalCreated": () => {
         withdrawalsCreated.push(curr);
       },
       "DepositCreated": function () {
         depositsCreated.push(curr);
       },
-      "WithdrawalApproved": function () {
+      "WithdrawalApproved": () => {
         const withdrawalCreated: Event | undefined = withdrawalsCreated.find(withdrawal => withdrawal.aggregateId === curr.aggregateId);
         if (withdrawalCreated && acc) acc.totalApprovedWithdrawalAmount += withdrawalCreated.body.amount ?? 0;
       },
-      "DepositApproved": function () {
+      "DepositApproved": () => {
         const depositCreated: Event | undefined = depositsCreated.find(deposit => deposit.aggregateId === curr.aggregateId);
         if (depositCreated && acc) acc.totalApprovedDepositAmount += depositCreated.body.amount ?? 0;
       }
