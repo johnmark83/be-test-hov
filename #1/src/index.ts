859834916ddd70e0c1a6b7f4c3874796a4991095
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-19 11:36:19 +0800

    feat: get total balance from account

--- #1/src/index.ts
+++ #1/src/index.ts
@@ -1,7 +1,17 @@
 import { AccountEvents } from '../../events';
 
 export function calculateAccountBalance(events: typeof AccountEvents, accountId: string): number {
-  return 0;
+  const totalCredit = events.reduce((acc, curr) => {
+    if (curr.aggregateId === accountId && curr.type === "BalanceCredited") return acc += curr.body.amount ?? 0;
+    return acc;
+  }, 0);
+
+  const totalDebit = events.reduce((acc, curr) => {
+    if (curr.aggregateId === accountId && curr.type === "BalanceDebited") return acc += curr.body.amount ?? 0;
+    return acc;
+  }, 0);
+
+  return totalCredit - totalDebit;
 }
 
 export function getAccountInformation(events: typeof AccountEvents, accountId: string) {
