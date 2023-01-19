66eb0e52835d7c65530e337c70d6fb39c765c4eb
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-19 13:00:45 +0800

    perf: create helper function for comparing id and event type

--- #1/src/index.ts
+++ #1/src/index.ts
@@ -1,15 +1,19 @@
 import { AccountEvents } from '../../events';
 
+function _isIdAndTypeEqual(id: string, type: string, itemId: string, itemType: string) {
+  return id === itemId && type === itemType;
+}
+
 export function calculateAccountBalance(events: typeof AccountEvents, accountId: string): number {
-  const totalCredit : number = events.reduce((acc, curr) => {
-    if (curr.aggregateId === accountId && curr.type === "BalanceCredited") return acc += curr.body.amount ?? 0;
+  const totalCredit: number = events.reduce((acc, curr) => {
+    if (_isIdAndTypeEqual(accountId, "BalanceCredited", curr.aggregateId, curr.type)) return acc += curr.body.amount ?? 0;
 
     return acc;
   }, 0);
 
-  const totalDebit : number = events.reduce((acc, curr) => {
-    if (curr.aggregateId === accountId && curr.type === "BalanceDebited") return acc += curr.body.amount ?? 0;
-    
+  const totalDebit: number = events.reduce((acc, curr) => {
+    if (_isIdAndTypeEqual(accountId, "BalanceDeposited", curr.aggregateId, curr.type)) return acc += curr.body.amount ?? 0;
+
     return acc;
   }, 0);
 
