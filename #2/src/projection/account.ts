3f736d147fc27e393ba015ceff801bff528dc912
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-20 16:53:03 +0800

    feat: create apply function for projection

--- #2/src/projection/account.ts
+++ #2/src/projection/account.ts
@@ -1,8 +1,10 @@
 import { AggregateType, Event } from '../../../events';
-import { IAccountInformation } from '../../../interfaces';
 import EventStore from '../library/eventstore';
 import Projection from '../library/projection';
-import { collections, connectToDatabase } from '../services/database';
+import { AccountModel } from '../models/AccountModel';
+import { DepositCreatedModel } from '../models/DepositCreatedModel';
+import { WithdrawalCreatedModel } from '../models/WithdrawalCreatedModel';
+import { collections } from '../services/database';
 
 export default class AccountProjection extends Projection {
   public constructor(eventStore: EventStore) {
@@ -17,22 +19,90 @@ export default class AccountProjection extends Projection {
   }
 
   protected async apply(event: Event) {
-    if (!collections.accounts) {
-      connectToDatabase()
-        .then(() => console.log("Hello"))
-        .catch(() => console.log("err"));
+    const query = { aggregateId: event.aggregateId };
+
+    let aggregateFunction = {
+      "AccountCreated": async () => {
+        try {
+          const newAccount: AccountModel = {
+            aggregateId: event.aggregateId,
+            email: event.body.email ?? "",
+            fullName: event.body.fullName ?? "",
+            password: event.body.password ?? "",
+            username: event.body.username ?? "",
+            totalApprovedDepositAmount: 0,
+            totalApprovedWithdrawalAmount: 0,
+            balance: 0
+          };
+
+          await collections.accounts?.insertOne(newAccount);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      "AccountUpdated": async () => {
+        await collections.accounts?.findOneAndUpdate(query, { $set: event.body });
+      },
+      "WithdrawalCreated": async () => {
+        try {
+          await collections.withdrawalsCreated?.insertOne(event);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      "DepositCreated": async () => {
+        try {
+          await collections.depositsCreated?.insertOne(event);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      "WithdrawalApproved": async () => {
+        try {
+          const withdrawalCreated = (await collections.withdrawalsCreated?.findOne(query)) as WithdrawalCreatedModel | undefined | null;
+
+          if (withdrawalCreated) {
+            await collections.accounts?.updateOne(
+              { aggregateId: withdrawalCreated.body.account },
+              { $inc: { totalApprovedWithdrawalAmount: withdrawalCreated.body.amount } });
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          await collections.withdrawalsCreated?.deleteOne(query);
+        }
+      },
+      "DepositApproved": async () => {
+        try {
+          const depositCreated = (await collections.depositsCreated?.findOne(query)) as DepositCreatedModel | undefined | null;
+
+          if (depositCreated) {
+            await collections.accounts?.updateOne(
+              { aggregateId: depositCreated.body.account },
+              { $inc: { totalApprovedDepositAmount: depositCreated.body.amount } });
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          await collections.depositsCreated?.deleteOne(query);
+        }
+      },
+      "BalanceCredited": async () => {
+        try {
+          await collections.accounts?.updateOne(query, { $inc: { balance: event.body.amount } });
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      "BalanceDebited": async () => {
+        try {
+          await collections.accounts?.updateOne(query, { $inc: { balance: -(event.body.amount) } });
+        } catch (error) {
+          console.error(error);
+        }
+      },
     }
 
-    try {
-      const accounts = (await collections.accounts?.find({}).toArray());
-      console.log(accounts);
-
-    } catch (error) {
-      console.log(error);
-    }
-    // TODO: Implement this method, to maintain a state in your database.
-    // You can choose any database of your own, but suggested is MongoDB.
-
-    // console.log(event);
+    if (aggregateFunction[event.type]) await aggregateFunction[event.type]();
   }
 }
