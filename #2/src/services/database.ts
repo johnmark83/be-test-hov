e532e5b21f5ee46698d28c39196d0b54ab7a65fa
johnmark83 <john.bautista@obf.ateneo.edu>
2023-01-20 16:50:06 +0800

    feat: add deposits and withdraws created to collection

--- #2/src/services/database.ts
+++ #2/src/services/database.ts
@@ -1,7 +1,7 @@
 import * as mongoDB from "mongodb";
 import { config } from "../config/config";
 
-export const collections: { accounts?: mongoDB.Collection } = {};
+export const collections: { accounts?: mongoDB.Collection, depositsCreated?: mongoDB.Collection, withdrawalsCreated?: mongoDB.Collection } = {};
 
 export async function connectToDatabase() {
     const client: mongoDB.MongoClient = new mongoDB.MongoClient(config.mongo.url);
@@ -11,8 +11,10 @@ export async function connectToDatabase() {
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
     const accountsCollection: mongoDB.Collection = db.collection(config.accountCollection.name);
+    const depositCreatedCollection: mongoDB.Collection = db.collection("DepositsCreated");
+    const withdrawalsCreatedCollection: mongoDB.Collection = db.collection("WithdrawalsCreated");
 
     collections.accounts = accountsCollection;
-
-    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${accountsCollection.collectionName}`);
+    collections.depositsCreated = depositCreatedCollection;
+    collections.withdrawalsCreated = withdrawalsCreatedCollection;
 }
